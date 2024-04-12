c0cfee3ca3b2b28dfee699eb7b60a2313a1f3202
korneliushen <korneliussen06@gmail.com>
2024-04-12 12:26:25 +0200

    Rettet opp feil fra forrige commit

--- nettside/actions.js
+++ nettside/actions.js
@@ -1,6 +1,6 @@
 "use server";
 export async function getLink() {
-  const res = await fetch("https://geoguessr-extension-nettside.vercel.app", {
+  const res = await fetch("https://geoguessr-extension-api.vercel.app/link", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
     cache: "no-store",
