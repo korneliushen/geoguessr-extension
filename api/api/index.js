d57b0ae8ee246da95a504ea1dc11977813bef968
korneliushen <korneliussen06@gmail.com>
2024-04-13 22:43:38 +0200

    Gjør klart til publisering og rate limit

--- api/api/index.js
+++ api/api/index.js
@@ -2,9 +2,20 @@ import express, { json } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import client from "./db.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 const port = 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
 
 app.use(json());
 app.use(cors());
--- extension/content.js
+++ extension/content.js
@@ -9,17 +9,13 @@ async function getH1() {
     console.log(linkArray);
     linkArray.push(link);
     console.log(linkArray);
-    if (!linkArray.includes(link)) {
-      await fetch("http://172.31.25.157:3000", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ link }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    } else {
-      console.log("Party already started");
-    }
+    await fetch("https://geoguessr-extension-api.vercel.app/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ link }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   } else {
     console.log("No input field detected, or party already started");
   }
