1c2e45d7f7bb4ef93a325d89ff4ee4e54c723516
korneliushen <korneliussen06@gmail.com>
2024-04-13 23:20:37 +0200

    Justering av rate limit

--- api/api/index.js
+++ api/api/index.js
@@ -8,8 +8,8 @@ const app = express();
 const port = 3000;
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
