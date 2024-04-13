7d118237594b51b65c4d512e4719690694a9928e
korneliushen <korneliussen06@gmail.com>
2024-04-14 00:01:35 +0200

    La til privacy side og mer klargjøring

---
+++ nettside/src/app/privacy/page.jsx
@@ -0,0 +1,9 @@
+export default async function Privacy() {
+    return (
+      <>
+        <main className="flex justify-center items-center h-screen w-screen bg-black">
+            <span className="flex w-80 h-44 text-white">Nettsiden/extension "IMGG" samler ingen persondata. Extension henter kun lenke til geoguessr party ved aktivering av extension.</span>
+        </main>
+      </>
+    )
+  }
